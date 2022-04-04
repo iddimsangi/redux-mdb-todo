9f97767934960c6b9a822fb47b87c5bca952175c
iddimsangi <adammsangi@gmail.com>
2022-04-04 10:55:18 +0300

    improve: add mark complete functionality

--- src/redux/reducers/addReducer.js
+++ src/redux/reducers/addReducer.js
@@ -25,7 +25,18 @@ export const addReducer = (state = initialState, { type, payload }) => {
       return {
         tasks: [...state.tasks, payload],
       };
-
+    case actionsTypes.MARK_COMPLETE_TASK:
+      let taskObj = state.tasks.find((task) => task.id === payload);
+      let taskObjIndex = state.tasks.findIndex((task) => task.id === payload);
+      const taskCopy = state.tasks;
+      taskCopy[taskObjIndex] = {
+        ...taskObj,
+        isActive: !taskObj.isActive,
+      };
+      console.log(taskObj);
+      return {
+        tasks: [...taskCopy],
+      };
     default:
       return state;
   }
