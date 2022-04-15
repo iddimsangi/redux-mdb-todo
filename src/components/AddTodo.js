dc08e176e41f1c3e161969253b438ab666785cef
iddimsangi <adammsangi@gmail.com>
2022-04-15 10:49:15 +0300

    improve: add active class when clicked

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -50,7 +50,9 @@ const AddTodo = () => {
                 <ul className="nav nav-tabs mb-4 pb-2" id="ex1" role="tablist">
                   <li className="nav-item" role="presentation">
                     <a
-                      className="nav-link active"
+                      className={
+                        filterKeyWord === "All" ? "nav-link active" : "nav-link"
+                      }
                       id="ex1-tab-1"
                       data-mdb-toggle="tab"
                       href="#ex1-tabs-1"
@@ -64,7 +66,11 @@ const AddTodo = () => {
                   </li>
                   <li className="nav-item" role="presentation">
                     <a
-                      className="nav-link"
+                      className={
+                        filterKeyWord === "Active"
+                          ? "nav-link active"
+                          : "nav-link"
+                      }
                       id="ex1-tab-2"
                       data-mdb-toggle="tab"
                       href="#ex1-tabs-2"
@@ -78,7 +84,11 @@ const AddTodo = () => {
                   </li>
                   <li className="nav-item" role="presentation">
                     <a
-                      className="nav-link"
+                      className={
+                        filterKeyWord === "Complete"
+                          ? "nav-link active"
+                          : "nav-link"
+                      }
                       id="ex1-tab-3"
                       data-mdb-toggle="tab"
                       href="#ex1-tabs-3"
